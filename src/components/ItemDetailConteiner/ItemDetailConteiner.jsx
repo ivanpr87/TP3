93671103c1a9dd65e211e828120b6fd33aff6884
ivanpr87 <ivanbastos18@gmail.com>
2022-10-09 20:01:18 -0300

    revise proyecto1

--- src/components/ItemDetailConteiner/ItemDetailConteiner.jsx
+++ src/components/ItemDetailConteiner/ItemDetailConteiner.jsx
@@ -18,19 +18,6 @@ export default function ItemDetailConteiner() {
     const [selectedItem, setSelectedItem] = useState()  
     const [load, setLoad] = useState(true)
 
-
-/*   useEffect(() => {
-        const getItemData = new Promise((resolve) => {
-            setTimeout(() => resolve(DataProductos.find(Productos => Productos.id === Number(id))), 2000)
-
-        });
-
-        getItemData.then((response) => setItem(response));
-    }, [id]);
-
-*/
-
-//tambien lo dejo para que se vea como estaba antes
 const getSelected = async (idItem) => {
     try {
         const document = doc(db, "productos", idItem)
